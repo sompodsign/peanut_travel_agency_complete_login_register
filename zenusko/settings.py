806dd4cf5c35defc241f9d2e270e2c152b0a5f3c
Shampad Sharkar <sompodsign@gmail.com>
2020-10-29 11:06:21 +0600

    complete

--- zenusko/settings.py
+++ zenusko/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'zenusko',
         'USER': 'postgres',
-        'PASSWORD': '12345',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
